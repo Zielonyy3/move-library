247e69f3bdaf08ea9a4d39daf3f1922171ff981f
Zielonyy3 <zielak134@gmail.com>
2020-06-06 20:17:30 +0200

    First try to do space making system

--- app.js
+++ app.js
@@ -11,8 +11,27 @@ class Movables {
             this.objectList.push(object);
         })
     }
-    recreateList() {
+    recreateList(movedObject) {
+        const tempObjectList = this.objectList.map(el => el);
+        this.objectList.length = 0;
 
+        console.log(movedObject);
+        tempObjectList.splice(movedObject.objectIndex, 1);
+        console.warn(movedObject.newIndex);
+
+        tempObjectList.forEach((obj, i) => {
+            if (i != movedObject.newIndex)
+                this.objectList.push(obj);
+            else {
+                this.objectList.push(movedObject);
+                this.objectList.push(obj);
+            }
+        })
+
+        this.objectList.forEach((el, i) => {
+            el.domElement.textContent = i;
+            console.log(el.domElement);
+        })
     }
 }
 
@@ -21,6 +40,7 @@ class MoveObject {
         this.creator = creator;
         this.moving = 0;
         this.objectIndex = objectIndex;
+        this.newIndex;
         this.moveElementListenerBind = this.moveElement.bind(this);
 
         this.domElement = element;
@@ -36,6 +56,7 @@ class MoveObject {
     }
 
     moveIt(e) {
+        console.log(this.domElement.offsetHeight);
         this.creator.objectList.forEach(el => el.domElement.style.backgroundColor = 'red');
 
         this.elementBound = this.domElement.getBoundingClientRect();
@@ -49,21 +70,19 @@ class MoveObject {
         this.newPositionTop = e.clientY - this.pointY - this.offsetTop;
 
         if (this.moving) {
-            console.log('odłączam');
             this.moving = 0;
             this.domElement.style.cursor = 'default';
             window.removeEventListener('mousemove', this.eventBindHandler)
-
-
-            // console.log(`Offset end: ${this.offsetTop}`);
+            const movedObject = this;
+            this.creator.recreateList(movedObject);
         } else {
-            console.log(this.pointY);
-            // console.log(`Offset start: ${this.offsetTop}`);
-            console.log('podłączam')
             this.moving = 1;
             this.domElement.style.cursor = 'grab';
             this.domElement.style.position = 'relative';
+            this.domElement.style.zIndex = 1;
+            this.domElement.style.transition = '0s';
             window.addEventListener('mousemove', this.eventBindHandler)
+            console.log();
         }
     }
 
@@ -76,15 +95,40 @@ class MoveObject {
 
     checkPosition() {
         console.log(this.creator.objectList[1].offsetTop);
+        let index = this.objectIndex;
         this.creator.objectList.forEach((el, i) => {
-            if (this.newPositionTop + this.offsetTop > el.offsetTop) {
-                el.domElement.style.backgroundColor = 'green';
-                this.objectIndex = i;
-                this.domElement.textContent = this.objectIndex;
-            } else {
-                el.domElement.style.backgroundColor = 'red';
+            if (i != this.objectIndex && i != this.objectIndex + 1) {
+                el.domElement.style.position = 'relative';
+                el.domElement.style.top = '0';
             }
+
+            if (this.newPositionTop + this.offsetTop - this.domElement.offsetHeight / 2 > el.offsetTop) {
+                if (i != this.objectIndex) {
+                    el.domElement.style.top = '-3em';
+                }
+                index = i;
+            } else {
+                if (el.objectIndex != i + 1) {
+                    el.domElement.style.backgroundColor = 'red';
+                    this.creator.objectList[i + 1].domElement.style.backgroundColor = 'blue';
+                }
+                el.domElement.style.top = '3em';
+            }
+
+            if (i == index)
+                el.domElement.style.backgroundColor = 'green';
+
+
+
         })
+        // if (index != this.objectIndex) {
+        //     // this.creator.objectList[index].domElement.style.position = 'relative';
+        //     this.creator.objectList[index].domElement.style.top = '-3em';
+        //     this.creator.objectList[index + 1].domElement.style.top = '3em';
+        // }
+
+        this.newIndex = index;
+        this.domElement.textContent = this.newIndex;
     }
 
 }
