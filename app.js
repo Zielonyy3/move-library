766afbb2ce3e1b338eb1306a5db5e4e4761a1026
Zielonyy3 <zielak134@gmail.com>
2020-06-06 19:14:54 +0200

    New code for moving and recognizning position

--- app.js
+++ app.js
@@ -12,16 +12,7 @@ class Movables {
         })
     }
     recreateList() {
-        this.moveList.textContent = '';
-        this.objectList.forEach(el => {
-            const li = document.createElement('li');
-            li.classList.add(el.domElement.classList);
-            const i = document.createElement('i');
-            i.classList.add('fas');
-            i.classList.add('fa-bars');
-            li.appendChild(i);
-            this.moveList.appendChild(li);
-        })
+
     }
 }
 
@@ -30,83 +21,72 @@ class MoveObject {
         this.creator = creator;
         this.moving = 0;
         this.objectIndex = objectIndex;
-        this.newIndex = this.objectIndex;
-
+        this.moveElementListenerBind = this.moveElement.bind(this);
 
         this.domElement = element;
         this.elementBars = this.domElement.querySelector('.fa-bars');
+
         this.elementBound = this.domElement.getBoundingClientRect();
-        this.startPositionLeft;
-        this.startPositionTop;
-        this.offsetLeft;
-        this.offsetTop;
-        this.lastLeft = this.elementBound.left;
-        this.lastTop = this.elementBound.top;
+        this.offsetTop = this.elementBound.top;
+        this.offsetLeft = this.elementBound.left;
+
+        this.eventBindHandler = this.moveElement.bind(this);
 
         this.domElement.addEventListener('click', this.moveIt.bind(this))
     }
 
     moveIt(e) {
         this.creator.objectList.forEach(el => el.domElement.style.backgroundColor = 'red');
-        // this.creator.recreateList();
 
         this.elementBound = this.domElement.getBoundingClientRect();
-        this.startPositionLeft = e.clientX - (e.clientX - this.elementBound.left);
-        this.startPositionTop = e.clientY - (e.clientY - this.elementBound.top);
 
-        this.offsetLeft = e.clientX - this.elementBound.left;
-        this.offsetTop = e.clientY - this.elementBound.top;
+        this.offsetTop = this.elementBound.top;
+        this.offsetLeft = this.elementBound.left;
 
-        this.lastLeft = 1 * this.domElement.style.left.slice(0, this.domElement.style.left.indexOf('px'));
-        this.lastTop = 1 * this.domElement.style.top.slice(0, this.domElement.style.top.indexOf('px'));
+        this.pointY = e.clientY - this.offsetTop;
+        this.pointX = e.clientX - this.offsetLeft;
 
-
+        this.newPositionTop = e.clientY - this.pointY - this.offsetTop;
 
         if (this.moving) {
+            console.log('odłączam');
             this.moving = 0;
             this.domElement.style.cursor = 'default';
+            window.removeEventListener('mousemove', this.eventBindHandler)
+
+
+            // console.log(`Offset end: ${this.offsetTop}`);
         } else {
+            console.log(this.pointY);
+            // console.log(`Offset start: ${this.offsetTop}`);
+            console.log('podłączam')
             this.moving = 1;
             this.domElement.style.cursor = 'grab';
+            this.domElement.style.position = 'relative';
+            window.addEventListener('mousemove', this.eventBindHandler)
         }
-        this.domElement.style.position = 'relative';
-        window.addEventListener('mousemove', (e) => {
-            if (this.moving == 1) {
-                console.log(this.newIndex);
-                // console.log(`clientX: ${e.clientX}, left: ${this.startPositionLeft}, offsetLeft: ${this.offsetLeft}, lastLeft: ${this.lastLeft} = ${e.clientX - this.startPositionLeft - this.offsetLeft + this.lastLeft*1}`);
-                const newPositionLeft = e.clientX - this.startPositionLeft - this.offsetLeft + this.lastLeft;
-                const newPositionTop = e.clientY - this.startPositionTop - this.offsetTop + this.lastTop;
-                this.domElement.style.left = `${newPositionLeft}px`;
-                this.domElement.style.top = `${newPositionTop}px`;
-                this.newIndex = this.checkPosition(newPositionLeft, newPositionTop);
-            }
-        })
-        console.warn(this.newIndex);
-        const newArray = this.creator.objectList.slice(0, this.newIndex);
-        console.log(newArray);
     }
 
-    checkPosition(newPositionLeft, newPositionTop) {
-        let newIndex;
+    moveElement(e) {
+        this.newPositionTop = e.clientY - this.pointY - this.offsetTop;
+        this.domElement.style.top = this.newPositionTop + 'px';
+        this.checkPosition()
+        // this.domElement.style.left = `${e.clientX - this.clickX - this.offsetLeft}px`;
+    }
+
+    checkPosition() {
+        console.log(this.creator.objectList[1].offsetTop);
         this.creator.objectList.forEach((el, i) => {
-            if (newPositionTop + this.startPositionTop > el.lastTop) {
-                newIndex = i;
-                this.elementBars.textContent = newIndex;
-                if (this != el) {
-                    el.domElement.style.backgroundColor = 'green';
-                    // console.log(`Mniejszy od ${i}`);
-                }
+            if (this.newPositionTop + this.offsetTop > el.offsetTop) {
+                el.domElement.style.backgroundColor = 'green';
+                this.objectIndex = i;
+                this.domElement.textContent = this.objectIndex;
             } else {
-                el.domElement.style.backgroundColor = 'blue'
+                el.domElement.style.backgroundColor = 'red';
             }
-        });
-        return newIndex;
-
-
-
+        })
     }
 
 }
-
 
 const moving = new Movables('dupa');
